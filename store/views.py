99cbc00aeb0d336477fe2a52f6610b529ae65f11
Michael Carbonari <mikecarbonari1@gmail.com>
2024-04-03 00:30:22 -0500

    Dynamically build products into album

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from . models import Category
+from . models import Category, Product
 
 
 def store_index(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all
+    context = {'all_products': all_products}
+    return render(request, 'store/store.html', context=context)
 
 
 def categories(request):
