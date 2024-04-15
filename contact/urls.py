37b7857d81cf8f8690f97c81c901eb39b0d1a46b
Michael Carbonari <mikecarbonari1@gmail.com>
2024-04-14 21:40:53 -0500

    Rename

--- contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'contact'
-
-urlpatterns = [
-    # Inquiry page Url
-    path('', views.contact_form, name='contact_url'),
-]
--- contact/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def contact_form(request):
-    """
-    view function for displaying the Inquiry Form
-    """
-    return render(request, 'contact/inquiry-form.html')
