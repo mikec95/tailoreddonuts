ed9f06a4002519ff01de70e878f9bccffaf02fe0
Michael Carbonari <mikecarbonari1@gmail.com>
2024-05-14 19:17:36 -0500

    Finish Inquiry form

--- inquiry/views.py
+++ inquiry/views.py
@@ -1,28 +1,39 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
 from .models import InquiryForm
+from django.urls import reverse
 
 
 def inquiry_form(request):
     """
-    view function for displaying the Inquiry Form
+    View function for displaying the Inquiry Form.
     """
     return render(request, 'inquiry/inquiry-form.html')
 
 
 def submit_form(request):
     if request.method == 'POST':
-        # Process form data here
-        # Create a new instance of the InquiryForm model
-        form_data = InquiryForm(
-            first_name=request.POST.get('first-name'),
-            last_name=request.POST.get('last-name'),
-            phone_number=request.POST.get('phone-number'),
-            email=request.POST.get('email'),
-            special_requests=request.POST.get('special-requests')
-        )
-        form_data.save()
+        try:
+            # Process form data here
+            # Create a new instance of the InquiryForm model
+            form_data = InquiryForm(
+                first_name=request.POST.get('first-name'),
+                last_name=request.POST.get('last-name'),
+                phone_number=request.POST.get('phone-number'),
+                email=request.POST.get('email'),
+                special_requests=request.POST.get('special-requests')
+            )
+            form_data.save()
 
-        # Redirect to a success page or another URL
-        return render(request, 'inquiry/inquiry-form.html')
+            # Add a success message
+            messages.success(
+                request, 'Thank you! We will be in touch with you soon!')
+
+            # Redirect to the same page after form submission
+            return HttpResponseRedirect(reverse('inquiry:inquiry_url'))
+        except Exception as e:
+            # Handle any exceptions here
+            messages.error(request, f'Whoops! An error occurred: {e}')
+
+    # Render the form page for GET requests or if form submission fails
+    return render(request, 'inquiry/inquiry-form.html')
