181f2696793347121453904bd8de638a01fc20fc
Michael Carbonari <mikecarbonari1@gmail.com>
2024-05-15 20:17:13 -0500

    gallery stuff

--- gallery/views.py
+++ gallery/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
 
-from home.models import Product
-
-# Create your views here.
-
 
 def gallery_index(request):
     """
     View function for rendering the gallery page.
     """
-    # Retrieve all products from the database
-    all_products = Product.objects.all()
-    # Define the context to be passed to the template
-    context = {'all_products': all_products}
-    # Render the HTML template with the provided context
-    return render(request, 'gallery/gallery.html', context=context)
+    # Render the HTML template
+    return render(request, 'gallery/gallery.html')
--- home/urls.py
+++ home/urls.py
@@ -5,13 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     # Main Page Url
-    path('', views.home_index, name='home'),
-
-    # Individual Product Url
-    path('product/<slug:product_slug>',
-         views.product_info, name='product-info'),
-
-    # Individual Category Url
-    path('category/<slug:category_slug>',
-         views.list_category, name="list-category")
+    path('', views.home_index, name='home')
 ]
